2e6b31d5f7efc1c0221397862880c59b9b53aba5
sergi <sergi@srenevic.net>
2023-05-08 13:17:39 +0200

    config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  paths: { tests: "tests" },
 };
 
 export default config;
